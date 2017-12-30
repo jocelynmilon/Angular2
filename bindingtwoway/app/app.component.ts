d5e929580504f2182427bec0a5cad248e034d202
jocelyn <jocelyn.milon@gmail.com>
2017-12-30 01:17:42 +0100

    intialisation bindingtwoway2.

--- bindingtwoway/app/app.component.ts
+++ bindingtwoway/app/app.component.ts
@@ -2,6 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'app/app.Component.html',
+    styleUrls: ['app/app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+
+   titre:string = 'two way binding';
+   texte:string = "";
+   taches:any[] = [
+       {texte: 'renover le centre entrainement', fait:false },
+       { texte: 'investir chez les jeunes joueur', fait: false },
+       { texte: 'acheter des stars', fait: false },     
+     ];
+
+
+
+
+
+
+
+   ajoutertache(nouvelleTache:string) {
+       this.taches.push(nouvelleTache)
+       this.texte = "";
+   }
+   tacherealise(index:number) {
+        console.log(this.taches[index] + " réalisé")
+   }
+
+ }
