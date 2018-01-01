f908105e25152788431223a418e381cd3a70fb59
jocelyn <jocelyn.milon@gmail.com>
2018-01-01 21:53:37 +0100

    fin directive

--- directive/app/app.component.ts
+++ directive/app/app.component.ts
@@ -1,7 +1,40 @@
 import { Component } from '@angular/core';
 
+class Langage {
+    id:number;
+    langue:string;
+    interesse:boolean;
+}
+
+const LANGAGES : Langage[] = [
+
+    { id: 1, langue: 'francais', interesse:false },
+    { id: 2, langue: 'brésilien', interesse: false },
+    { id: 3, langue: 'espagnole', interesse: false },
+    { id: 4, langue: 'allemand', interesse: false },
+    { id: 5, langue: 'argentin', interesse: false },
+
+]
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'app/app.component.html',
+    styleUrls: ['app/app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+
+    titre:string = 'directive';
+
+    langages = LANGAGES;
+
+    surLigner(index:number) {
+       
+        this.langages[index].interesse = (this.langages[index].interesse == false ? this.langages[index].interesse = true : this.langages[index].interesse = false)
+        
+        
+    }
+        
+        
+      
+    ];
+ }
