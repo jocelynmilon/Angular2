baf5dc4bdea6e9191c7710881484a0aac20eba55
jocelyn <jocelyn.milon@gmail.com>
2018-01-10 19:57:10 +0100

    template2

--- templateUrl/app/app.component.ts
+++ templateUrl/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AppFormulaireComponent } from './app.formulaire.component'
+import { Todo } from './todo';
+import { TODOS } from './todos';
 
 
 @Component({
@@ -12,5 +14,30 @@ export class AppComponent {
    
     titre:string ="templateUrl";
     
+    todos = TODOS;
+    todoSelectionne: Todo;
+    edit:boolean = false;
+
+    setClasses(todo: Todo) {
+        let classes = {
+            faible: todo.priority == 'faible',
+            moyenne: todo.priority == 'moyenne',
+            haute : todo.priority == 'haute'
+        };
+        return classes
+    }
     
+    onSelect(todo: Todo) {
+        this.todoSelectionne = todo;
+        console.log(todo)
+
+    }
+    modifierTodo(bool:boolean) {
+        if(bool) {
+            this.edit = false;
+        }else {
+            this.edit = true;
+        }
+
+    }
 }
--- templateUrl/app/app.formulaire.component.ts
+++ templateUrl/app/app.formulaire.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Todo } from './todo';
+import { TODOS } from './todos';
 
 @Component({
     selector: 'formulaire-todo',
@@ -9,13 +10,8 @@ import { Component } from '@angular/core';
 export class AppFormulaireComponent {
 
 
-    todos: any[] = [
-        { texte: 'aller au FC Barcelone', desc: 'rencontrer Neymar', date: new Date(2017, 6, 30), estFait: false, priority: 'haute' },
-        { texte: 'aller à As Monaco', desc: 'rencontrer Mbappé', date: new Date(2017, 7, 5), estFait: false, priority: 'haute' },
-        { texte: 'aller à la Juventus turin', desc: 'rencontrer Alves', date: new Date(2017, 7, 1), estFait: false, priority: 'haute' },
-        { texte: 'aller au PEC Zwolle', desc: 'rencontrer Hebling', date: new Date(2017, 7, 17), estFait: false, priority: 'faible' },
-        { texte: 'aller au Pescara Calcio', desc: 'rencontrer Bahebeck', date: new Date(2017, 8, 17), estFait: false, priority: 'moyenne' },
-    ]
+    todos = TODOS;
+    priorities:string[] = ['faible','moyenne', 'haute'];
 
 
     ajouterTodo(texte: string, desc: string, date: Date, priority: string) {
